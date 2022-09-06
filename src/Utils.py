8fdb5f40077a13a1a518e171332cd826b16d31a9
Daniel Kaiser <kaiserd@iu.edu>
2022-09-06 15:27:57 -0400

    Reorganize, prepare ex05v1.5, ex04v2.0

--- src/Utils.py
+++ src/Utils.py
@@ -1,77 +1,29 @@
-import networkx as nx
-import random
-from sklearn import metrics
-from cdlib import algorithms
-from cdlib import evaluation
-import numpy as np
-import matplotlib.pyplot as plt
-import time
-from scipy import stats
-import os
-from tqdm import tqdm as tqdm
-import pickle
+# ============= SET-UP =================
+# --- Standard library ---
+import os  # for calling shell scripts
+import random  # for random shufflings
+
+# --- Scientific ---
+import numpy as np  # General computational tools
+from sklearn import metrics, decomposition  # Measuring classifier performance
+
+# --- Network science ---
+import networkx as nx  # General network tools
+from node2vec import Node2Vec as N2V  # Embedding tools
+import mercator  # Mercator code (see bin)
+
+# --- Data handling and visualization ---
+import pandas as pd  # Dataframe tools
+from tabulate import tabulate  # Pretty printing for dataframes
+
+# --- Miscellaneous ---
+from tqdm.auto import tqdm  # Progress bar
 
 
-def generate_power_law (gamma, kmin, kmax):
-    xmin = np.power(kmin, 1.0 - gamma)
-    xmax = np.power(kmax, 1.0 - gamma)
-    x = xmax - random.random()*(xmax - xmin)
-    x = np.power(x, 1.0 / (1.0 -gamma))
-    return int(x)
-
-######
-
-def control_correlation (degree, prob):
-    tmp_degree = []
-    for i in range(len(degree)):
-        tmp_degree.append(degree[i])
-
-    for i in range(len(tmp_degree)):
-        if random.random()<prob:
-            n = tmp_degree[i]
-            j = random.randint(0, len(degree)-1)
-            tmp_degree[i] = tmp_degree[j]
-            tmp_degree[j] = n
-
-    return tmp_degree
-
-#####
-
-def generate_configuration_model (degree):
-    edges = []
-    for i in range(0, len(degree)):
-        k = degree[i]
-        for j in range(0, k):
-            edges.append(i)
-
-    ##random.shuffle(edges)
-    for i in range(0, len(edges)):
-        j = random.randint(0, len(edges)-1)
-        tmp = edges[i]
-        edges[i] = edges[j]
-        edges[j] = tmp
-
-    G = nx.Graph()
-    for n in range(0, len(degree)):
-        G.add_node(n)
-
-    for i in range(0, len(edges)-1, 2):
-        n = edges[i]
-        m = edges[i+1]
-        if G.has_edge(n, m) == False and n != m:
-            G.add_edge(n, m)
-
-    return G
-
-################
-
+# =================== FUNCTIONS ===================
+# --- Drivers ---
+# ~ Configuration model ~
 def generate_multiplex_configuration (N, gamma, kmin, kmax, prob, sign):
-#     print ('# N = ', N)
-#     print ('# gamma = ', gamma)
-#     print ('# kmin = ', kmin)
-#     print ('# kmax = ', kmax)
-#     print ('# prob = ', prob)
-#     print ('# sign = ' , sign)
     degree = []
     for i in range(0, N):
         degree.append(generate_power_law (gamma, kmin, kmax))
@@ -91,9 +43,25 @@ def generate_multiplex_configuration (N, gamma, kmin, kmax, prob, sign):
 
     return G
 
+# ~ Community model ~
+def LFR(n,t1,t2,mu,avg_k,max_k):
+    #function to generate LFR network as a networkx object and obtain community assignments
+    N,Mu,T1,T2,maxk,k=str(n),str(mu),str(t1),str(t2),str(max_k),str(avg_k)
+    s='../bin/LFR/benchmark -N '+N+' -mu '+Mu+ ' -maxk ' +maxk  + ' -k '+k  +' -t1 ' +T1+' -t2 ' +T2
+    os.system(s)
 
-#########
+    x=np.loadtxt('network.dat')
+    edges=[(int(x[i][0])-1,int(x[i][1])-1) for i in range(len(x))]
+    g=nx.Graph(edges)
 
+    x=np.loadtxt('community.dat')
+    coms={int(x[i][0])-1:int(x[i][1]) for i in range(len(x))}
+    #nx.set_node_attributes(g,coms,name='community')
+
+    return g, coms
+
+# --- Experiment helpers ---
+# ~ General experiment set up ~
 def duplex_network (G, l1, l2):
     G1 = G[l1].copy()
     G2 = G[l2].copy()
@@ -138,12 +106,7 @@ def duplex_network (G, l1, l2):
 
     return G1, G2
 
-
 def partial_information (G1, G2, frac):
-
-
-#     print ('# option = ', option)
-
     ##training/test sets
     Etest = {}
     Etrain = {}
@@ -159,8 +122,6 @@ def partial_information (G1, G2, frac):
             Etrain[e] = 0
         else:
             Etest[e] = 0
-
-
 
     ##remnants
     rem_G1 = nx.Graph()
@@ -189,32 +150,34 @@ def partial_information (G1, G2, frac):
 
     return rem_G1, rem_G2, Etest
 
+# ~ Individual layer generators ~
+def generate_configuration_model (degree):
+    edges = []
+    for i in range(0, len(degree)):
+        k = degree[i]
+        for j in range(0, k):
+            edges.append(i)
 
+    ##random.shuffle(edges)
+    for i in range(0, len(edges)):
+        j = random.randint(0, len(edges)-1)
+        tmp = edges[i]
+        edges[i] = edges[j]
+        edges[j] = tmp
 
-def LFR(n,t1,t2,mu,avg_k,max_k):
-    #function to generate LFR network as a networkx object and obtain community assignments
-    N,Mu,T1,T2,maxk,k=str(n),str(mu),str(t1),str(t2),str(max_k),str(avg_k)
-    s='../bin/LFR/benchmark -N '+N+' -mu '+Mu+ ' -maxk ' +maxk  + ' -k '+k  +' -t1 ' +T1+' -t2 ' +T2
-    os.system(s)
+    G = nx.Graph()
+    for n in range(0, len(degree)):
+        G.add_node(n)
 
-    x=np.loadtxt('network.dat')
-    edges=[(int(x[i][0])-1,int(x[i][1])-1) for i in range(len(x))]
-    g=nx.Graph(edges)
+    for i in range(0, len(edges)-1, 2):
+        n = edges[i]
+        m = edges[i+1]
+        if G.has_edge(n, m) == False and n != m:
+            G.add_edge(n, m)
 
-    x=np.loadtxt('community.dat')
-    coms={int(x[i][0])-1:int(x[i][1]) for i in range(len(x))}
-    #nx.set_node_attributes(g,coms,name='community')
-
-    return g, coms
-
-
-
+    return G
 
 def lfr_multiplex (N, tau1, tau2, mu, average_degree, max_degree, min_community, prob_relabel):
-
-
-    #############################
-
     H, comm = LFR(n=N, t1=tau1, t2=tau2, mu=mu, avg_k=average_degree, max_k = max_degree)
     groups = {}
     for n in comm:
@@ -223,14 +186,9 @@ def lfr_multiplex (N, tau1, tau2, mu, average_degree, max_degree, min_community,
             groups[c] = []
         groups[c].append(n)
 
-   #############################
-
     sigma1 = {}
     for n in comm:
         sigma1[n] = comm[n]
-
-   #########
-
 
     new_labels = {}
     for C in groups:
@@ -240,17 +198,11 @@ def lfr_multiplex (N, tau1, tau2, mu, average_degree, max_degree, min_community,
             n = groups[C][i]
             m = tmp[i]
             new_labels[n] = m
-    ###########################################
 
     tmp_sigma2 = {}
     for n in sigma1:
         m = new_labels[n]
         tmp_sigma2[m] = sigma1[n]
-
-    ###########################################
-
-
-
 
     G = {}
     G[1] = H.copy()
@@ -261,16 +213,9 @@ def lfr_multiplex (N, tau1, tau2, mu, average_degree, max_degree, min_community,
         n = new_labels[e[0]]
         m = new_labels[e[1]]
         G[2].add_edge(n, m)
-    ############################################
 
 
-
-
-
-
-    #############################################
     ## break community correlation
-
     list_nodes = list(G[2].nodes())
     new_labels = {}
     H = G[2].copy()
@@ -291,21 +236,33 @@ def lfr_multiplex (N, tau1, tau2, mu, average_degree, max_degree, min_community,
         n = new_labels[e[0]]
         m = new_labels[e[1]]
         G[2].add_edge(n, m)
-    ##############################################
-
 
     sigma2 = {}
     for n in tmp_sigma2:
         m = new_labels[n]
         sigma2[m] = tmp_sigma2[n]
 
-
-
-#     print(len(G[1].edges()))
-#     print(len(G[2].edges()))
-
-
-
     return G, sigma1, sigma2, mu
 
 
+# --- Computation helpers ---
+def generate_power_law (gamma, kmin, kmax):
+    xmin = np.power(kmin, 1.0 - gamma)
+    xmax = np.power(kmax, 1.0 - gamma)
+    x = xmax - random.random()*(xmax - xmin)
+    x = np.power(x, 1.0 / (1.0 -gamma))
+    return int(x)
+
+def control_correlation (degree, prob):
+    tmp_degree = []
+    for i in range(len(degree)):
+        tmp_degree.append(degree[i])
+
+    for i in range(len(tmp_degree)):
+        if random.random()<prob:
+            n = tmp_degree[i]
+            j = random.randint(0, len(degree)-1)
+            tmp_degree[i] = tmp_degree[j]
+            tmp_degree[j] = n
+
+    return tmp_degree

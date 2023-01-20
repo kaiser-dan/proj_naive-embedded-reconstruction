65db053ba6047a603e1d93964f508fba08ca4fcb
Daniel Kaiser <dkaiser4897@gmail.com>
2023-01-20 12:57:57 -0500

    Reorganization

--- results/concat_df.py
+++
@@ -1,8 +0,0 @@
-import os
-import pandas as pd
-
-dfs = [pd.read_parquet(f"dataframes/{f}") for f in os.listdir("dataframes/") if "EMB_ex04R" in os.path.basename(f)]
-
-df = pd.concat(dfs)
-
-df.to_parquet("dataframes/dataframe-merged_EMB_ex04R_DK_20220928.parquet")

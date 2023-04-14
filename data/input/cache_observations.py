7dd12fb7895998f6818807b9e26b89b8aef7b078
Daniel Kaiser <dkaiser4897@gmail.com>
2023-04-14 10:51:48 -0400

    rsync with KJ

--- data/input/cache_observations.py
+++ data/input/cache_observations.py
@@ -4,6 +4,7 @@
 # --- Standard library ---
 import sys
 from itertools import product
+from enum import Enum
 
 # --- Scientific computing ---
 import numpy as np
@@ -19,6 +20,25 @@ from utils import parameters as params
 # --- Miscellaneous ---
 from tqdm import tqdm
 
+
+# --- Globals ---
+class Config(float, Enum):
+    # LFR params
+    N = 1000.0  # convert to int
+    AVG_K = 6.0
+    T1 = 2.1
+    T2 = 1.0
+    PROB = 1.0
+    MU = 0.1
+    # Observations
+    REPS = 1.0  # convert to int
+    THETA_MIN = 0.05
+    THETA_MAX = 0.95
+    THETA_NUM = 37.0  # convert to int
+    # Resources
+    WORKERS = 60.0  # convert to int
+
+
 # ================ FUNCTIONS ======================
 def main(args):
     # >>> Book-keeping >>>
@@ -32,33 +52,37 @@ def main(args):
     else:
         systems = [
             (
-                f"LFR_mu-{mu}_prob-{prob}",
+                f"LFR_gamma-{T1}",
                 1, 2,
                 preprocessing.duplex_network(
                     benchmarks.lfr_multiplex(
-                        10_000,
-                        2.1, 1.0,
-                        mu, 20,
-                        np.sqrt(10_000),
-                        1,
-                        prob)[0],
+                        int(Config.N.value),
+                        T1, Config.T2.value,
+                        Config.MU.value,
+                        Config.AVG_K.value, np.sqrt(Config.N.value) if T1 <= 3 else np.power(Config.N.value, 1/(T1-1)),  # <k>, max(k)
+                        1,  # min_community - ignored downstream
+                        Config.PROB.value)[0],
                     1, 2
                 )
             )
-            for mu in [0.1] #, 0.2, 0.3, 0.4, 0.5]
-            for prob in [0.0] #, 0.25, 0.5, 0.75, 1.0]
+            for T1 in [2.1, 2.5, 2.9, 3.5]
         ]
 
 
-    reps = range(1, 3+1)
-    thetas = np.linspace(0.05, 0.95, 19, endpoint=True)
-    parameters, hyperparameters, _ = params.set_parameters_N2V(workers=16)
+    reps = range(1, int(Config.REPS.value)+1)
+    thetas = np.linspace(Config.THETA_MIN.value, Config.THETA_MAX.value, int(Config.THETA_NUM.value), endpoint=True)
+    parameters, hyperparameters, _ = params.set_parameters_N2V(workers=int(Config.WORKERS.value))
     # <<< Book-keeping <<<
 
     grid = list(product(systems, thetas, reps))
+    print(f"Discovered {len(grid)} parameter grid vertices")
 
     for system_, theta, rep in tqdm(grid, desc="Caching observational data..."):
         system, l1, l2, (G, H) = system_
+
+        # ! >>> DEBUG >>>
+        print(f"DEBUG - System={system}; layers=({l1}, {l2}); rep={rep}")
+        # ! <<< DEBUG <<<
 
         observations.calculate_preprocessed_data(
             G, H,

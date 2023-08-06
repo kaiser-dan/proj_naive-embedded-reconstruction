ffcdac71299c4ae68c7e8f275922b8a63373e023
Daniel Kaiser <dkaiser4897@gmail.com>
2023-08-05 22:24:51 -0400

    Set description as dynamic metadata

--- src/embmplxrec/__init__.py
+++ src/embmplxrec/__init__.py
@@ -1,1 +1,3 @@
-VERSION = "0.11.2"
+VERSION = "1.0.0"
+DESCRIPTION = "Tools for the reconstruction of multiplex networks from partial structural observations via graph embeddings."
+LONG_DESCRIPTION = "[long description]"
